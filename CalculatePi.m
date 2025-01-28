330801a260783a57d64d780270e15281799db7f7
paolobardella <paolo.bardella@gmail.com>
2025-01-28 13:43:37 +0100

    4

--- CalculatePi.m
+++ CalculatePi.m
@@ -3,7 +3,7 @@ clear;                          %clear
 close;       
 
 %close all figures
-NumSamples=1000;                    %Number of samples to be generated
+NumSamples =1000;                    %Number of samples to be generated
 L=1;                                %Square side [A.U.]
 R=L/2;                              %Radius
 Inside=0;                           %Number of points inside the circle
